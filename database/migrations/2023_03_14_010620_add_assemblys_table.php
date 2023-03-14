6d489d6741d6d1ecef8fe98b685398350a3b0120
= <57159295+JulianRamirezJ@users.noreply.github.com>
2023-03-13 20:15:07 -0500

    Migrations for order,item,part and assembly created

---
+++ database/migrations/2023_03_14_010620_add_assemblys_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('assemblys', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('bike_id');
+            $table->foreign('bike_id')->references('id')->on('bikes');
+            $table->unsignedBigInteger('part_id');
+            $table->foreign('part_id')->references('id')->on('parts');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('assemblys');
+    }
+};
