b543b6995a99374586df308800b0e780c9557d25
= <57159295+JulianRamirezJ@users.noreply.github.com>
2023-03-14 20:54:04 -0500

    Enum for role

--- database/migrations/2023_03_13_232909_add_fields_to_users_table.php
+++ database/migrations/2023_03_13_232909_add_fields_to_users_table.php
@@ -12,7 +12,10 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('role');
+            $table->enum(
+                'role',
+                ['admin','user']
+            );
             $table->string('address');
             $table->integer('balance');
         });
